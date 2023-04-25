0905540c46b89386a988a2c7c39c0c6a07ed915a
Prince Modi <princemodi@iisc.ac.in>
2023-04-25 13:02:40 +0530

    Batched.py bug fix

--- batched.py
+++ batched.py
@@ -6,11 +6,12 @@ import time
 
 parser=argparse.ArgumentParser()
 parser.add_argument("-n","--Name",help="Model name")
+parser.add_argument("-b","--Batch",help="Batch size")
 args=parser.parse_args()
 model = args.Name
 
 # Batch size
-batch=2
+batch=int(args.Batch)
 
 # Load a model
 m1=time.time()
@@ -18,25 +19,24 @@ model = YOLO(model)  # load an official model
 m2=time.time()
 
 images=glob.iglob("resized/*.jpg")
-print(images)
 g1=time.time()
 
 print("img,class,conf,class-name,read,run,write")
 
 for j in range(int(1000/batch)):
-#    img=image[8:-4]
-    i1=time.time()
+    fetch_start=time.time()
     jpeg=[]
     name=[]
     for i in range(batch):
         name.append(next(images))
-        jpeg.append(cv2.imread(next(images)))
-    i2=time.time()
+        jpeg.append(cv2.imread(name[i]))
+    fetch_done=time.time()
+    run_start=time.time()
     results = model.predict(jpeg, conf=0.5)  # predict on an image
-    r1=time.time()
-    print(len(results))
+    run_done=time.time()
     # print(f'Class:{cls}, Conf:{conf}')
     for k in range(batch):
+        post_start=time.time()
         boxes = results[k].boxes
         cls=boxes.cls.to('cpu').numpy()
         conf=boxes.conf.to('cpu').numpy()
@@ -47,5 +47,5 @@ for j in range(int(1000/batch)):
         img=results[k].plot(conf=True, labels=True, boxes=True, masks=True, probs=True)
         jpg=name[k][8:]
         cv2.imwrite(f"results/{jpg}",img)
-        w1=time.time()
-        print(f"{jpg},{cls},{conf},{names},{i2-i1:0,.5f},{r1-i2:0,.5f},{w1-r1:0,.5f}")
+        post_done=time.time()
+        print(f"{jpg},{cls},{conf},{names},{fetch_done-fetch_start:0,.5f},{run_done-run_start:0,.5f},{post_done-post_start:0,.5f}")
